f00150f72154b6bf7eef05c7ca0b8ab18bbe8f8a
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-01-27 16:06:18 -0500

    work on dynamic

--- d3_data_journalism/StarterCode/app.js
+++ d3_data_journalism/StarterCode/app.js
@@ -23,42 +23,85 @@ var chosenXAxis = "poverty";
 
 function xScale(data, chosenXAxis) {
     var xLinearScale = d3.scaleLinear()
-        .domain([d3.min(data, d => d[chosenXaxis]) * 0.8,
+        .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
             d3.max(data, d => d[chosenXAxis]) * 1.2
         ])
-        .range([0, width])
-    return xLinearScale
+        .range([0, width]);
+    return xLinearScale;
 }
-function renderAxes(newXScale, )
+function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+    return xAxis;
+}
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis])); 
+    return circlesGroup;
+  }
+function updateToolTip(chosenXAxis, circlesGroup) {
+    var label;
+    if (chosenXAxis === "poverty") {
+      label = "In Poverty (%):";
+    }
+    else if (chosenXAxis === "age") {
+      label = "Age (Median):";
+    }
+    else if (chosenXAxis === "income") {
+        label = "Household Income (Median)"
+    }
+  
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+    });
 
+    chartGroup.call(toolTip);  
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+          toolTip.hide(data);
+        });
+    return circlesGroup;
+}
+    
 d3.csv("data.csv").then(function(data) {
     console.log(data)
     data.forEach(function(sample) {
         sample.healthcare = +sample.healthcare;
         sample.poverty = +sample.poverty;
+        sample.age = +sample.age;
+        sample.income = +sample.income;
     });
 
-    var xLinearScale = d3.scaleLinear()
-    .domain([8, d3.max(data, d => d.poverty)])
-    .range([0, width]);
+    var xLinearScale = xScale(data, chosenXAxis);
     var yLinearScale = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.healthcare)])
       .range([height, 0]);
 
-      var bottomAxis = d3.axisBottom(xLinearScale);
+    var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
 
     chartGroup.append("g")
       .call(leftAxis);
+
     var circlesGroup = chartGroup.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cx", d => xLinearScale(d[chosenXAxis]))
       .attr("cy", d => yLinearScale(d.healthcare))
       .attr("r", "20")
       .classed("stateCircle", true);
@@ -77,22 +120,8 @@ d3.csv("data.csv").then(function(data) {
         })
         .classed("stateText", true);  
     
-    var toolTip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healthcare}%`);
-      });
-
-
-    chartGroup.call(toolTip);  
-    circlesGroup.on("mouseover", function(data) {
-        toolTip.show(data, this);
-      })
-        // onmouseout event
-        .on("mouseout", function(data, index) {
-          toolTip.hide(data);
-        });
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`);
     
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
@@ -102,8 +131,72 @@ d3.csv("data.csv").then(function(data) {
         .attr("class", "axisText")
         .text("Lacks Healthcare (%)");
       
-    chartGroup.append("text")
-        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    var povertyLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 20)
+        .attr("value", "poverty")
         .attr("class", "axisText")
+        .classed("active", true)
         .text("In Poverty (%)");
-  })
+    var ageLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "age")
+        .attr("class", "axisText")
+        .classed("inactive", true)
+        .text("Age (Median)");
+    var incomeLabel = labelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 60)
+        .attr("value", "income")
+        .attr("class", "axisText")
+        .classed("inactive", true)
+        .text("Household Income (Median)");
+    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+    labelsGroup.selectAll("text")
+        .on("click", function() {
+            var value = d3.select(this).attr("value");
+            if (value !== chosenXAxis) {
+                chosenXAxis = value;
+                xLinearScale = xScale(data, chosenXAxis);
+                xAxis = renderAxes (xLinearScale, xAxis);
+                circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+                circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+                if (chosenXAxis === "age") {
+                    ageLabel
+                        .classed("active", true)
+                        .classed("inacive", false);
+                    povertyLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    incomeLabel
+                        .classed("active", false)
+                        .classed("inactive", true)
+                }
+                else if (chosenXAxis === "income") {
+                    incomeLabel
+                        .classed("active", true)
+                        .classed("inacive", false);  
+                    ageLabel
+                        .classed("active", false)
+                        .classed("inactive", true);   
+                    povertyLabel
+                        .classed("active", false)
+                        .classed("inactive", true);                                                  
+                }
+                else {
+                    povertyLabel
+                        .classed("active", true)
+                        .classed("inacive", false); 
+                    incomeLabel 
+                        .classed("active", false)
+                        .classed("inactive", true); 
+                    ageLabel
+                        .classed("active", false)
+                        .classed("inactive", true);                
+                }
+            }
+        });
+  }).catch(function(error) {
+    console.log(error);
+  });
